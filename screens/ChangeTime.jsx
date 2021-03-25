42b47c0dfcd22a7ae3adb2a4516c5ab3c5607c1c
Adrian Palomares <adrianpal45@gmail.com>
2021-03-25 16:34:06 -0700

    layout change

--- screens/ChangeTime.jsx
+++ screens/ChangeTime.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import { Button, SafeAreaView } from 'react-native';
 import PropTypes from 'prop-types';
+import React from 'react';
+import { Button, SafeAreaView, Text, TextInput, View, StyleSheet } from 'react-native';
 
-const ChangeTimeScreen = ({ navigation }) => (
+const styles = StyleSheet.create({
+  minutesTextInput: {
+    backgroundColor: '#fff',
+    height: 30,
+    width: '50%',
+  },
+});
+
+const ChangeTimeScreen = ({ navigation, route }) => (
+  // route.params.setCurrentTime(0);
   <SafeAreaView>
-    <Button title="Next Screen" onPress={() => navigation.navigate('Change Time')} />
+    {/* <Button title="Next Screen" onPress={() => navigation.navigate('Change Time')} /> */}
+    <View>
+      <Text>Select Minutes</Text>
+      <TextInput
+        style={styles.minutesTextInput}
+        keyboardType="numeric"
+        keyboardAppearance="dark"
+        returnKeyType="done"
+      />
+    </View>
+    <View>
+      <Button title="Set Minutes" />
+    </View>
   </SafeAreaView>
 );
 
 ChangeTimeScreen.propTypes = {
   navigation: PropTypes.objectOf(PropTypes.object).isRequired,
+  route: PropTypes.objectOf(PropTypes.object).isRequired,
 };
 
 export default ChangeTimeScreen;
--- screens/PomodoroScreen.jsx
+++ screens/PomodoroScreen.jsx
@@ -2,12 +2,27 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { SafeAreaView, Text, Button } from 'react-native';
 
-const PomodoroScreen = ({ navigation }) => (
-  <SafeAreaView>
-    <Button title="Next Screen" onPress={() => navigation.navigate('ChangeTimeScreen')} />
-    <Text>Pomodoro Screen</Text>
-  </SafeAreaView>
-);
+const PomodoroScreen = ({ navigation }) => {
+  // Timer
+  const [currentTime, setCurrentTime] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(currentTime + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentTime]);
+
+  return (
+    <SafeAreaView>
+      <Button title="Next Screen" onPress={() => navigation.navigate('ChangeTimeScreen', { currentTime, setCurrentTime })} />
+      <Text>{currentTime}</Text>
+    </SafeAreaView>
+  );
+};
 
 PomodoroScreen.propTypes = {
   navigation: PropTypes.objectOf(PropTypes.object).isRequired,
