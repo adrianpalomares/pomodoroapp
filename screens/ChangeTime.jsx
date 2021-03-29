efaece50792c3b4ebe2f80e959da7b62278ede63
Adrian Palomares <adrianpal45@gmail.com>
2021-03-28 19:05:40 -0700

    changed settings screen button

--- screens/ChangeTime.jsx
+++ screens/ChangeTime.jsx
@@ -2,6 +2,7 @@ import { useTheme } from '@react-navigation/native';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Button, SafeAreaView, Text, TextInput, View, StyleSheet } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
   centering: {
@@ -15,6 +16,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     height: 30,
     width: '50%',
+  },
+  actionContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonStyle: {
+    backgroundColor: '#007AFF',
+    borderRadius: 20,
+    padding: 10,
   },
 });
 
@@ -91,9 +102,9 @@ const ChangeTimeScreen = ({ navigation, route }) => {
           returnKeyType="done"
         />
       </View>
-      <View>
+      <View style={styles.actionContainer}>
         {/* TODO: Handle empty value, null by default */}
-        <Button
+        {/* <Button
           title="Set Minutes"
           // TODO: Add break time update, also using wrong value
           onPress={() => {
@@ -101,7 +112,17 @@ const ChangeTimeScreen = ({ navigation, route }) => {
             route.params.setBreakTime(convertToSeconds(breakMinuteValue, breakSecondValue));
             alert('Time updated!');
           }}
-        />
+        /> */}
+        <TouchableWithoutFeedback
+          style={styles.buttonStyle}
+          onPress={() => {
+            route.params.setPomodoroTime(convertToSeconds(pomodoroMinuteValue, pomodoroSecondValue));
+            route.params.setBreakTime(convertToSeconds(breakMinuteValue, breakSecondValue));
+            alert('Time updated!');
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 25 }}>Apply</Text>
+        </TouchableWithoutFeedback>
       </View>
     </SafeAreaView>
   );
